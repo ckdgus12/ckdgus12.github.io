eb82f65361e24c065a018c4909bef1efb02ed043
ckdgus12 <ckdgus1308@naver.com>
2023-03-11 23:40:05 +0900

    첼로

---
+++ js/vitality.js
@@ -0,0 +1,164 @@
+/*!
+ * Vitality v1.4.0 (http://themes.startbootstrap.com/vitality-v1.4.0)
+ * Copyright 2013-2016 Start Bootstrap Themes
+ * To use this theme you must have a license purchased at WrapBootstrap (https://wrapbootstrap.com)
+ */
+
+// Load WOW.js on non-touch devices
+var isPhoneDevice = "ontouchstart" in document.documentElement;
+$(document).ready(function() {
+    if (isPhoneDevice) {
+        //mobile
+    } else {
+        //desktop               
+        // Initialize WOW.js
+        wow = new WOW({
+            offset: 50
+        })
+        wow.init();
+    }
+});
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // Smooth Scrolling: Smooth scrolls to an ID on the current page.
+    // To use this feature, add a link on your page that links to an ID, and add the .page-scroll class to the link itself. See the docs for more details.
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Activates floating label headings for the contact form.
+    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+    }).on("focus", ".floating-label-form-group", function() {
+        $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function() {
+        $(this).removeClass("floating-label-form-group-with-focus");
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Owl Carousel Settings
+    $(".about-carousel").owlCarousel({
+        items: 3,
+        navigation: true,
+        pagination: false,
+        navigationText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
+    });
+
+    $(".portfolio-carousel").owlCarousel({
+        singleItem: true,
+        navigation: true,
+        pagination: false,
+        navigationText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
+        autoHeight: true,
+        mouseDrag: false,
+        touchDrag: false,
+        transitionStyle: "fadeUp"
+    });
+
+    $(".testimonials-carousel, .mockup-carousel").owlCarousel({
+        singleItem: true,
+        navigation: true,
+        pagination: true,
+        autoHeight: true,
+        navigationText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
+        transitionStyle: "backSlide"
+    });
+
+    $(".portfolio-gallery").owlCarousel({
+        items: 3,
+    });
+
+    // Magnific Popup jQuery Lightbox Gallery Settings
+    $('.gallery-link').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        },
+        image: {
+            titleSrc: 'title'
+        }
+    });
+
+    $('.mix').magnificPopup({
+        type: 'image',
+        image: {
+            titleSrc: 'title'
+        }
+    });
+
+    // Formstone Background - Video Background Settings
+    $("header.video").background({
+        source: {
+            poster: "img/agency/backgrounds/bg-mobile-fallback.jpg",
+            mp4: "mp4/camera.mp4"
+        }
+    });
+
+    // Scrollspy: Highlights the navigation menu items while scrolling.
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    })
+
+    // Portfolio Filtering Scripts & Hover Effect
+    var filterList = {
+        init: function() {
+
+            // MixItUp plugin
+            $('#portfoliolist').mixItUp();
+
+        },
+
+    };
+
+    filterList.init();
+
+})(jQuery); // End of use strict
+const backToTop = document.getElementById('backtotop');
+
+function checkScroll() {
+    /*
+      웹페이지가 수직으로 얼마나 스크롤되었는지를 확인하는 값(픽셀 단위로 반환)
+      https://developer.mozilla.org/ko/docs/Web/API/Window/pageYOffset
+    */
+    let pageYOffset = window.pageYOffset;
+
+    if (pageYOffset !== 0) {
+        backToTop.classList.add('show');
+    } else {
+        backToTop.classList.remove('show');
+    }
+
+}
+
+function moveBackToTop() {
+    if (window.pageYOffset > 0) {
+        /*
+        smooth 하게 스크롤하기
+        https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollTo
+        */
+       window.scrollTo({top: 0, behavior: "smooth"})
+    }
+}
+
+window.addEventListener('scroll', checkScroll);
+backToTop.addEventListener('click', moveBackToTop);
